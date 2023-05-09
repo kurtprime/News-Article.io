fb3879c84520cb3a4ec5cc425a112fa4115da754
refocus-wd-course <127702749+refocus-wd-course@users.noreply.github.com>
2023-05-09 16:24:37 +0200

    Add files via upload

---
+++ src/components/PostImage.jsx
@@ -0,0 +1,11 @@
+export const PostImage = ({ props }) => {
+  // TODO: the code below is just for demo,
+  // so feel free to delete it and write your own.
+  // Hint: You can pass the props and update that post upon saving
+
+  return (
+    <form>
+      <button onClick={addImage}>Add Image</button>
+    </form>
+  );
+};

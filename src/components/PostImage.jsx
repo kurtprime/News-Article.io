5215c3df526944808196c5f3fd651a7ffb46c578
John De Leon <john.deleon@simplyrfid.com>
2023-05-10 13:26:29 +0800

    Add react uploader

--- src/components/PostImage.jsx
+++ src/components/PostImage.jsx
@@ -1,11 +1,57 @@
-export const PostImage = ({ props }) => {
-  // TODO: the code below is just for demo,
-  // so feel free to delete it and write your own.
-  // Hint: You can pass the props and update that post upon saving
+import { useState, useRef } from "react";
+import LoadingSpinner from "./LoadingSpinner";
+import { uploadImage } from "../plugins/uploadImage";
+
+export const PostImage = ({ addImageSuccessful }) => {
+  // This code sends an API to a 3rd party library that uploads and serves the image
+  // Added a spinner for user-experience and this component returns an image {fileURL}
+  // Do not touch any of the code
+
+  const imageInput = useRef();
+  const [image, setImage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleImageUpload = async (e) => {
+    try {
+      const { fileUrl } = await uploadImage.uploadFile(e.target.files[0], {
+        onProgress,
+      });
+      imageInput.current.value = "";
+      setImage(fileUrl);
+      addImageSuccessful(fileUrl);
+    } catch (e) {
+      console.warn(`Error: ${e}`);
+    }
+    setIsLoading(false);
+  };
+  const onProgress = ({ progress }) => {
+    setIsLoading(true);
+    console.log(`File uploading: ${progress}% complete.`);
+  };
 
   return (
-    <form>
-      <button onClick={addImage}>Add Image</button>
-    </form>
+    <div
+      className="image-uploader"
+      style={{
+        backgroundImage: image ? `url(${image})` : "#f8f9fc",
+        backgroundSize: "cover",
+      }}
+    >
+      {isLoading ? <LoadingSpinner /> : ""}
+      <label
+        className="image-button"
+        style={{
+          display: image ? "none" : "block",
+        }}
+      >
+        <input
+          type="file"
+          name="image_upload"
+          onChange={handleImageUpload}
+          ref={imageInput}
+        />
+        + Add Image
+      </label>
+    </div>
   );
 };
---
+++ src/plugins/uploadImage.js
@@ -0,0 +1,4 @@
+import { Upload } from "upload-js";
+export const uploadImage = Upload({
+  apiKey: "public_kW15bJo9qDLmY5vdde1z22Vjn8nx",
+});
